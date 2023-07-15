8324858488c9c8603d53ed9c08e84e67cbe34acf
Gustavo Carvalho <gusfcarvalho@gmail.com>
2023-07-15 11:49:03 -0300

    Small fixes
    Signed-off-by: Gustavo Carvalho <gusfcarvalho@gmail.com>

--- operator/internal/provider/checkov/checkov.go
+++ operator/internal/provider/checkov/checkov.go
@@ -2,12 +2,13 @@ package checkov
 
 import (
 	"fmt"
+	"os"
+
+	"os/exec"
 
 	argusiov1alpha1 "github.com/ContainerSolutions/argus/operator/api/v1alpha1"
 	provider "github.com/ContainerSolutions/argus/operator/internal/provider/schema"
-	"io/ioutil"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"os/exec"
 )
 
 type Client struct {
@@ -19,63 +20,40 @@ type Client struct {
 func (c *Client) Attest() (argusiov1alpha1.AttestationResult, error) {
 	// ToDo: generate unique file names for clone_location and output_file_path
 	clone_location := "/tmp/location"
-	output_file_path := "/tmp/output"
-
-	var result_type argusiov1alpha1.AttestationResultType
-
 	cmd := exec.Command("git", "clone", c.RepoUrl, clone_location)
-	err := cmd.Run()
+	defer os.RemoveAll("/tmp/location")
+	_, err := cmd.CombinedOutput()
 
 	if err != nil {
-		fmt.Println(err)
-		result_type = argusiov1alpha1.AttestationResultTypeFail
-
 		res := argusiov1alpha1.AttestationResult{
-			Result: result_type,
+			Result: argusiov1alpha1.AttestationResultTypeUnknown,
 			Logs:   err.Error(), // Logs has to be type string
 			RunAt:  v1.Now(),
-			Reason: fmt.Sprintf("checkov provider configured for '%v'", result_type),
+			Reason: fmt.Sprintf("could not get source repo for '%v'", c.RepoUrl),
 		}
-		// ToDo: delete clone_location
 		return res, nil
 	}
 
-	// checkov command
-	checkov_cmd := exec.Command("checkov", "-d", clone_location, "--check", c.Checks, "--output-file-path", output_file_path)
+	checkov_cmd := exec.Command("checkov", "-d", clone_location, "--check", c.Checks, "-o", "cli")
 
-	err = checkov_cmd.Run()
+	out, err := checkov_cmd.CombinedOutput()
 
 	// Distinguish between execution and validation failure
-	if err != nil {
-		fmt.Println(err)
-		result_type = argusiov1alpha1.AttestationResultTypeFail
-
-		res := argusiov1alpha1.AttestationResult{
-			Result: result_type,
-			Logs:   err.Error(), // Logs has to be type string
-			RunAt:  v1.Now(),
-			Reason: fmt.Sprintf("checkov provider configured for '%v'", result_type),
-		}
-		// ToDo: delete output_file
-		return res, nil
-	} else {
-		result_type = argusiov1alpha1.AttestationResultTypePass
-	}
-
-	content, err := ioutil.ReadFile(output_file_path) // content is of type []byte
-
-	if err != nil {
-		result_type = argusiov1alpha1.AttestationResultTypeFail
-		content = []byte(err.Error()) // Converted err to type []byte
-	}
-
 	res := argusiov1alpha1.AttestationResult{
-		Result: result_type,
-		Logs:   string(content), // Logs has to be type string
+		Result: argusiov1alpha1.AttestationResultTypePass,
+		Logs:   string(out),
 		RunAt:  v1.Now(),
-		Reason: fmt.Sprintf("checkov provider configured for '%v'", result_type),
+		Reason: "check pass",
 	}
-	// ToDo: delete clone_location
+	if err != nil {
+		res.Result = argusiov1alpha1.AttestationResultTypeUnknown
+		res.Err = err.Error()
+		res.Reason = "checkov execution returned error"
+	}
+	if checkov_cmd.ProcessState.ExitCode() != 0 {
+		res.Result = argusiov1alpha1.AttestationResultTypeFail
+		res.Reason = "checkov execution failed"
+	}
 	return res, nil
 }
 
@@ -87,7 +65,7 @@ type Provider struct{}
 
 func (p *Provider) New(spec *argusiov1alpha1.AttestationProviderSpec) (provider.AttestationClient, error) {
 	c := &Client{}
-	repourl_value, ok := spec.ProviderConfig["repourl"]
+	repourl_value, ok := spec.ProviderConfig["repo"]
 
 	if ok {
 		c.RepoUrl = repourl_value
